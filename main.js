0123ed47def7f29cdb386dde5a76831c91d22fc2
Madrog <rogers.madda@gmail.com>
2019-12-30 17:01:40 +0300

    added node packages

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ class BlockChain {
 
     addBlock(data) {
         let index = this.chain.length;
-        let prevHash = this.chain.length !== 0 ? this.chain[length - 1].hash : 0;
+        let prevHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : 0;
         let block = new Block(index, data, prevHash);
         this.chain.push(block);
     }
@@ -36,7 +36,7 @@ class BlockChain {
         for (var i = 0; i < this.chain.length; i++) {
             if (this.chain[i].hash !== this.chain[i].getHash())
                 return false;
-            if (i > 0 && this.chain[i].prevHash !== this.chain[i - j].hash)
+            if (i > 0 && this.chain[i].prevHash !== this.chain[i - 1].hash)
                 return false;
         }
         return true;
